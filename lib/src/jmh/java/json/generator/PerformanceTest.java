bd87b0c1386bfd3c18290631134d1714e76b88cb
psumiya <sumiya.p@gmail.com>
2022-12-20 22:40:45 -0800

    Add benchmark test and readme - missed files

---
+++ lib/src/jmh/java/json/generator/PerformanceTest.java
@@ -0,0 +1,42 @@
+package json.generator;
+
+import json.generator.model.JsonGeneratorModel;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+public class PerformanceTest {
+
+    private static final String FILE_NAME = "src/jmh/resources/samples/root_is_object.json";
+
+    private static final JsonGenerator<String, String> jsonGenerator = new JsonToJsonGenerator(new JsonGeneratorModel());
+
+    private static final String SAMPLE;
+
+    static {
+        Path path = Paths.get(FILE_NAME);
+        try {
+            SAMPLE = Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Benchmark
+    @Fork(value = 3, warmups = 2)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    @OperationsPerInvocation(10)
+    public String executeMethodToBenchmark() {
+        return jsonGenerator.generate(SAMPLE);
+    }
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+
+}
