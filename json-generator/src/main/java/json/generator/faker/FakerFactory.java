1868d8f6669ef20693dbd657ddcf122c55d7e9a1
psumiya <sumiya.p@gmail.com>
2023-02-11 09:12:08 -0800

    Works with grouped providers, though the values continue to be random

--- json-generator/src/main/java/json/generator/faker/FakerFactory.java
+++ json-generator/src/main/java/json/generator/faker/FakerFactory.java
@@ -3,34 +3,69 @@ package json.generator.faker;
 import com.fasterxml.jackson.databind.JsonNode;
 import json.generator.FieldGenerator;
 import json.generator.model.FieldConfiguration;
+import net.datafaker.AbstractProvider;
+import net.datafaker.Address;
 import net.datafaker.Faker;
+import net.datafaker.Name;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class FakerFactory {
 
     public static List<FieldGenerator> buildFakerGenerators(Faker faker) {
         FakerStringSupplier fakerStringSupplier = new FakerStringSupplier(faker);
+        Map<String, AbstractProvider> fakerGroupProviders = Map.of(
+                "name", faker.name(),
+                "address", faker.address(),
+                "business", faker.business(),
+                "book", faker.book()
+        );
         List<FieldGenerator> fakerGenerators = new ArrayList<>();
         for (FakerType fakerType : FakerType.values()) {
 
-            fakerGenerators.add(new FieldGenerator() {
-
-                @Override
-                public String getFieldName() {
-                    return fakerType.fieldKey;
-                }
-
-                @Override
-                public JsonNode generate(FieldConfiguration configuration, JsonNode seedValue) {
-                    return fakerStringSupplier.apply(() -> fakerType.supplier.apply(faker));
-                }
-
-            });
+            FieldGenerator fieldGenerator = Optional.ofNullable(fakerType.groupName)
+                    .map(groupName -> fakerGroupProviders.get(groupName))
+                    .map(abstractProvider -> getFieldGenerator(fakerStringSupplier, fakerType, abstractProvider))
+                    .orElse(getDefaultFieldGenerator(faker, fakerStringSupplier, fakerType));
+            fakerGenerators.add(fieldGenerator);
 
         }
         return fakerGenerators;
+    }
+
+    private static FieldGenerator getFieldGenerator(FakerStringSupplier fakerStringSupplier, FakerType fakerType, AbstractProvider abstractProvider) {
+        return new FieldGenerator() {
+
+            @Override
+            public String getFieldName() {
+                return fakerType.fieldKey;
+            }
+
+            @Override
+            public JsonNode generate(FieldConfiguration configuration, JsonNode seedValue) {
+                return fakerStringSupplier.apply(() -> fakerType.valueSupplier.apply(abstractProvider));
+            }
+
+        };
+    }
+
+    private static FieldGenerator getDefaultFieldGenerator(Faker faker, FakerStringSupplier fakerStringSupplier, FakerType fakerType) {
+        return new FieldGenerator() {
+
+            @Override
+            public String getFieldName() {
+                return fakerType.fieldKey;
+            }
+
+            @Override
+            public JsonNode generate(FieldConfiguration configuration, JsonNode seedValue) {
+                return fakerStringSupplier.apply(() -> fakerType.supplier.apply(faker));
+            }
+
+        };
     }
 
 }
--- json-generator/src/main/java/json/generator/faker/FakerType.java
+++ json-generator/src/main/java/json/generator/faker/FakerType.java
@@ -1,31 +1,31 @@
 package json.generator.faker;
 
-import net.datafaker.Faker;
+import net.datafaker.*;
 
 import java.util.function.Function;
 
 public enum FakerType {
 
-    FULL_NAME("fullName", faker -> faker.name().fullName()),
-    FIRST_NAME("firstName", faker -> faker.name().firstName()),
-    LAST_NAME("lastName", faker -> faker.name().lastName()),
+    FULL_NAME("fullName", "name", name -> ((Name) name).fullName()),
+    FIRST_NAME("firstName", "name", name -> ((Name) name).firstName()),
+    LAST_NAME("lastName", "name", name -> ((Name) name).lastName()),
 
     GENDER("gender", faker -> faker.gender().types()),
 
-    ADDRESS("address", faker -> faker.address().fullAddress()),
-    STREET_ADDRESS("streetAddress", faker -> faker.address().streetAddress()),
-    STATE("state", faker -> faker.address().state()),
-    TIME_ZONE("timeZone", faker -> faker.address().timeZone()),
-    CITY("city", faker -> faker.address().city()),
+    ADDRESS("address", "address", address -> ((Address) address).fullAddress()),
+    STREET_ADDRESS("streetAddress", "address", address -> ((Address) address).streetAddress()),
+    STATE("state", "address", address -> ((Address) address).state()),
+    TIME_ZONE("timeZone", "address", address -> ((Address) address).timeZone()),
+    CITY("city", "address", address -> ((Address) address).city()),
 
-    CREDIT_CARD_TYPE("creditCardType", faker -> faker.business().creditCardType()),
-    CREDIT_CARD_NUMBER("creditCardNumber", faker -> faker.business().creditCardNumber()),
-    CREDIT_CARD_EXPIRY("creditCardExpiry", faker -> faker.business().creditCardExpiry()),
+    CREDIT_CARD_TYPE("creditCardType", "business", business -> ((Business) business).creditCardType()),
+    CREDIT_CARD_NUMBER("creditCardNumber", "business", business -> ((Business) business).creditCardNumber()),
+    CREDIT_CARD_EXPIRY("creditCardExpiry", "business", business -> ((Business) business).creditCardExpiry()),
 
-    BOOK_TITLE("title", faker -> faker.book().title()),
-    BOOK_AUTHOR("author", faker -> faker.book().author()),
-    BOOK_GENRE("genre", faker -> faker.book().genre()),
-    BOOK_PUBLISHER("publisher", faker -> faker.book().publisher()),
+    BOOK_TITLE("title", "book", book -> ((Book) book).title()),
+    BOOK_AUTHOR("author", "book", book -> ((Book) book).author()),
+    BOOK_GENRE("genre", "book", book -> ((Book) book).genre()),
+    BOOK_PUBLISHER("publisher", "book", book -> ((Book) book).publisher()),
 
     COFFEE_BLEND("blend", faker -> faker.coffee().blendName()),
 
@@ -39,10 +39,34 @@ public enum FakerType {
 
     protected final String fieldKey;
     protected final Function<Faker, String> supplier;
+    protected final String groupName;
+    protected final Function<AbstractProvider, String> valueSupplier;
 
+    /**
+     * Use for ungrouped types
+     *
+     * @param fieldKey the fieldKey
+     * @param supplier the supplier function
+     */
     FakerType(String fieldKey, Function<Faker, String> supplier) {
         this.fieldKey = fieldKey;
         this.supplier = supplier;
+        this.groupName = null;
+        this.valueSupplier = null;
+    }
+
+    /**
+     * Use for grouped types
+     *
+     * @param fieldKey the fieldKey
+     * @param groupName the groupName
+     * @param valueSupplier the valueSupplier
+     */
+    FakerType(String fieldKey, String groupName, Function<AbstractProvider, String> valueSupplier) {
+        this.fieldKey = fieldKey;
+        this.supplier = null;
+        this.groupName = groupName;
+        this.valueSupplier = valueSupplier;
     }
 
 }
